fe36a740eedfce99ee8ac27ee60d33c78f55c8b2
Jeremy Beck <jeremycbeck@outlook.com>
2018-07-31 19:00:55 -0700

    About me Lab 2

--- js/app.js
+++ js/app.js
@@ -1,1 +1,57 @@
-console.log('js is linked');
+'use strict';
+
+alert('Do you know Jeremy?');
+var answerOne = prompt('Am I an only child? ').toLowerCase();
+console.log('User answer to Q1: ' + answerOne);
+
+if (answerOne === 'y' || answerOne === 'yes') {
+  alert('You are incorrect!');
+} else if (answerOne === 'n' || answerOne === 'no') {
+  alert('You are correct!');
+} else {
+  alert('Invalid response. Please enter yes(Y) or no(N).');
+}
+
+var answerTwo = prompt('Do I have brown eyes?').toLowerCase();
+console.log('User answer to Q2: ' + answerTwo);
+
+if (answerTwo === 'y' || answerTwo === 'yes') {
+  alert('You are correct!');
+} else if (answerTwo === 'n' || answerTwo === 'no') {
+  alert('You are incorrect!');
+} else {
+  alert('Invalid response. Please enter yes(Y) or no(N).');
+}
+
+var answerThree = prompt('Do I wear contacts?').toLowerCase();
+console.log('User answer to Q3: ' + answerThree);
+
+if (answerThree === 'y' || answerThree === 'yes') {
+  alert('You are incorrect!');
+} else if (answerThree === 'n' || answerThree === 'no') {
+  alert('You are correct!');
+} else {
+  alert('Invalid response. Please enter yes(Y) or no(N).');
+}
+
+var answerFour = prompt('Do I like soccer?').toLowerCase();
+console.log('User answer to Q4: ' + answerFour);
+
+if (answerFour === 'y' || answerFour === 'yes') {
+  alert('You are correct!');
+} else if (answerFour === 'n' || answerFour === 'no') {
+  alert('You are incorrect!');
+} else {
+  alert('Invalid response. Please enter yes(Y) or no(N).');
+}
+
+var answerFive = prompt('Have I reached the summit at Mt Rainier?').toLowerCase();
+console.log('User answer to Q5: ' + answerFive);
+
+if (answerFive === 'y' || answerFive === 'yes') {
+  alert('You are incorrect!');
+} else if (answerFive === 'n' || answerFive === 'no') {
+  alert('You are correct!');
+} else {
+  alert('Invalid response. Please enter yes(Y) or no(N).');
+}
