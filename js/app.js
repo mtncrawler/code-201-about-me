04476fc715b1dca6fe7a7ea0dacf4c7e60fc20af
Jeremy Beck <jeremycbeck@outlook.com>
2018-08-01 17:12:45 -0700

    Initial lab 3

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var correctAns = 0;
+
 var user = prompt('Welcome, what is your name?');
 console.log('User name is ' + user);
 
@@ -11,6 +13,7 @@ if (answerOne === 'y' || answerOne === 'yes') {
   alert('You are incorrect!');
 } else if (answerOne === 'n' || answerOne === 'no') {
   alert('You are correct!');
+  correctAns += 1;
 } else {
   alert('Invalid response. Please enter yes(Y) or no(N).');
 }
@@ -20,6 +23,7 @@ console.log('User answer to Q2: ' + answerTwo);
 
 if (answerTwo === 'y' || answerTwo === 'yes') {
   alert('You are correct!');
+  correctAns += 1;
 } else if (answerTwo === 'n' || answerTwo === 'no') {
   alert('You are incorrect!');
 } else {
@@ -33,6 +37,7 @@ if (answerThree === 'y' || answerThree === 'yes') {
   alert('You are incorrect!');
 } else if (answerThree === 'n' || answerThree === 'no') {
   alert('You are correct!');
+  correctAns += 1;
 } else {
   alert('Invalid response. Please enter yes(Y) or no(N).');
 }
@@ -42,6 +47,7 @@ console.log('User answer to Q4: ' + answerFour);
 
 if (answerFour === 'y' || answerFour === 'yes') {
   alert('You are correct!');
+  correctAns += 1;
 } else if (answerFour === 'n' || answerFour === 'no') {
   alert('You are incorrect!');
 } else {
@@ -55,6 +61,54 @@ if (answerFive === 'y' || answerFive === 'yes') {
   alert('You are incorrect!');
 } else if (answerFive === 'n' || answerFive === 'no') {
   alert('You are correct!');
+  correctAns += 1;
 } else {
   alert('Invalid response. Please enter yes(Y) or no(N).');
 }
+
+for(var i = 4; i > 0; i--) {
+  var answerSix = prompt('How many pizzas did I eat this week? You have ' + i + ' attempts remaining.');
+  console.log('User answer to Q6: ' + answerSix);
+  
+  if (answerSix < 6 && answerSix >= 0) {
+    alert('Too low! Try again.');
+  }
+  else if(answerSix > 6) {
+    alert('Too high! Try again.');
+  } 
+  else if(answerSix == 6) {
+    alert('You are correct!');
+    correctAns += 1;
+    break;
+  }
+  else {
+    alert('Please enter a positive integer.');
+  }
+}
+
+var states = ['georgia', 'south carolina', 'washington'];
+
+for(var j = 6; j > 0; j--) {
+  var answerSeven = prompt('Which states have I lived in? You have ' + j + ' attempts remaining.').toLowerCase();
+  console.log('User answer to Q6: ' + answerSeven);
+  
+  if (states.includes(answerSeven)) {
+    alert('You are correct! I have lived in Georgia, Washington, and South Carolina.');
+    correctAns += 1;
+    break;
+  }
+  else {
+    alert('Try again.');
+  }
+}
+
+if(correctAns > 5) {
+  alert('Well done ' + user + ', keep it up! You got ' + correctAns + ' out of 7 questions correct.');
+}
+else if(correctAns == 7) {
+  alert(user + ', perfect score! You clearly know Jeremy very well. You got ' + correctAns + ' out of 7 questions correct.');
+}
+else {
+  alert(user + ', better luck next time! You got ' + correctAns + ' out of 7 questions correct.');
+}
+
